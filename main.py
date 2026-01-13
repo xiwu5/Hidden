82622bedd6c4f9a73db7a19d403530d16673da79
Xi <youngjoycewu@gmail.com>
2026-01-13 23:18:47 +0800

    update

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
 def hidden(matrix, n):
-    # Your implementation here!
-    pass
+    # handle input validation
+
+    
+    #row = len(matrix)
+
+    # initialize the return string
+
+
+    
+    # char = ''
+    # index = 0
+    # for row in matrix:
+    #     for letter in row: 
+    #         if index % n == 0: 
+    #             char += letter
+    #         index += 1
+    # return char
+
+    char = matrix[0][0]
+    index = 0
+    for row in matrix:
+        for letter in row: 
+            # char += letter # addon first letter
+            # # addon index + n letter if index is not out of bound
+            while index + n < len(row):
+                print("current index", index)
+                char += row[index + n]
+                index += n
+        print(char)
+
+    return char
+
+    
 
 matrix_1 = (
     ('u','e','r','e', ' ', 'e'),
+    #0.       2.      4
     ('d', 'z', 'o', 'b', 'i', 'v'),
+    #0.        2.         4
     ('n',),
+    #0
+
     ('w', 'g', 'q', ' ', '5', 'g', 'w'),
+    # 0.        2.       4.         6
     ('r',),
     ('y', 'e'),
     ('u', 'a', 'u', 't')
